28533da8618085de907e83bb407281248aa2299d
Amanda G. Brown <a@agbr.dev>
2022-02-11 01:11:36 -0500

    User Story 1: Added a test for the getFileSections function

--- src/getFileSections.ts
+++ src/getFileSections.ts
@@ -9,19 +9,23 @@
 
 const hasValidLineLength = (line: string) => line.length === 27
 
-export default function getFileSections(lines: string[]) {
+export default function getFileSections(lines: string[]): string[][] {
 
     let entries = [];
 
     // for loop, skipping every 4 lines
     for (let i = 0; i < lines.length; i += 4) {
-        const accountLines = lines.slice(i, i + 3); //first through the an array of 3 strings
-        if (accountLines.every(hasValidLineLength)) { // check and make sure has is 27 characters long...
+        if (lines.length > i + 3) {
+            const accountLines = lines.slice(i, i + 3); //first through the an array of 3 strings
             entries.push(accountLines);
-        }
-        else {
-            //...otherwise, don't include it and alert the user.
-            console.error(`the account \n${accountLines.join('\n')} contains an invalid charater`);
+            if (accountLines.every(hasValidLineLength)) { // check and make sure has is 27 characters long...
+                entries.push(accountLines);
+            }
+            //(TODO: see why there's an invalid character. Commented out for now.)
+            // else {
+            //     //...otherwise, don't include it and alert the user.
+            //     console.error(`the account \n${accountLines.join('\n')} contains an invalid charater`);
+            // }
         }
 
     };
