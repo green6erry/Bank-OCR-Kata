681184da0d91919621d2b2136526a560bb52ec9f
Amanda G. Brown <a@agbr.dev>
2022-02-11 01:17:16 -0500

    User Story 1: TODO fix one test that is still not working

--- src/getFileSections.ts
+++ src/getFileSections.ts
@@ -17,15 +17,16 @@ export default function getFileSections(lines: string[]): string[][] {
     for (let i = 0; i < lines.length; i += 4) {
         if (lines.length > i + 3) {
             const accountLines = lines.slice(i, i + 3); //first through the an array of 3 strings
+            console.log('accountLines length', accountLines.length);
             entries.push(accountLines);
             if (accountLines.every(hasValidLineLength)) { // check and make sure has is 27 characters long...
                 entries.push(accountLines);
             }
             //(TODO: see why there's an invalid character. Commented out for now.)
-            // else {
-            //     //...otherwise, don't include it and alert the user.
-            //     console.error(`the account \n${accountLines.join('\n')} contains an invalid charater`);
-            // }
+            else {
+                //...otherwise, don't include it and alert the user.
+                console.error(`the account \n${accountLines.join('\n')} contains an invalid charater`);
+            }
         }
 
     };
--- test/bank-ocr-kata.test.ts
+++ test/bank-ocr-kata.test.ts
@@ -3,32 +3,30 @@ import getFileSections from '../src/getFileSections';
 
 const partialNumberLines = [
   ' _  _  _  _  _  _  _  _  _ ',
-  '_| _| _| _| _| _| _| _| _|',
+  ' _| _| _| _| _| _| _| _| _|',
   '|_ |_ |_ |_ |_ |_ |_ |_ |_ ',
-  '',
+  '    ',
   '_  _  _  _  _  _  _  _  _ ',
-  '_ | _ | _ | _ | _ | _ | _ | _ | _ | '
+  '_ | _ | _ | _ | _ | _ | _ | _ | _ | ',
+
 ];
 
 const partialNumberLinesResult = [
   [
     ' _  _  _  _  _  _  _  _  _ ',
-    '_| _| _| _| _| _| _| _| _|',
+    ' _| _| _| _| _| _| _| _| _|',
     '|_ |_ |_ |_ |_ |_ |_ |_ |_ '
   ]
 ];
 // 
 describe('User Story 1', () => {
-  test('but there is a "stop" in Christoph', () => {
-    expect('Christoph').toMatch(/stop/);
-  });
+  // TODO: Add test about file being found (use all the FS stuff);
+
 
   test('getFileSections: account numbers are being parsed', () => {
     expect(getFileSections(partialNumberLines)).toEqual(partialNumberLinesResult);
-
   })
 
-  // TODO: Add test about file being found (use all the FS stuff);
 
 });
 
